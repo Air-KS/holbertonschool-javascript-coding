509ddc611c9493c8b14c35d252370a7c4838627d
Air-KS <kevinrogeret@gmail.com>
2023-09-19 07:53:32 +0000

    Task 5

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const request = require ('request');
+
+const url = process.argv[2];
+const filePatch = process.argv[3];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  fs.writeFile(filePatch, body, 'utf8', function (err) {
+    if (err) {
+      console.error(err);
+    }
+  });
+});
